db47eed644519896449611fecd81cd41d0c8b29f
Viktor Koda <vkoda@luxoft.com>
2013-03-21 14:13:29 +0200

    Added include for win32 build

--- ogl_es.h
+++ ogl_es.h
@@ -1,12 +1,15 @@
-// Define this for SDL build
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
-#ifdef USE_SDL
+#ifdef USE_SDL // Define this for SDL build
 #include <GLES2/gl2.h>
 typedef char GLchar; // Some GLES headers do not typedef this
 #else
 #include <GL/glew.h>
+#ifdef _WIN32
+#include <GL/wglew.h>
+#else
 #include <GL/glx.h>
-#endif
+#endif // _WIN32
+#endif // USE_SDL
