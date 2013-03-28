e5a57208274c7818fc2ba54422c7cecfb23ed525
Viktor Koda <vkoda@luxoft.com>
2013-03-28 19:59:58 +0200

    Comments added.

--- ogl_es.h
+++ ogl_es.h
@@ -2,13 +2,16 @@
 #include <windows.h>
 #endif
 
-#if defined(LG_TV)
+#ifdef USE_EGL
+// LG TV
 #   include <GLES2/gl2.h>
 #   include <EGL/egl.h>
 #elif defined(USE_SDL)
+// HT Touchpad
 #   include <GLES2/gl2.h>
 typedef char GLchar; // Some GLES headers do not typedef this
-#else
+#else // X11
+// Desktop Linux
 #   include <GL/glew.h>
 #   include <GL/glx.h>
 #endif
